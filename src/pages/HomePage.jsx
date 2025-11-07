8b962ece72d6f5e07935670fa19f85b5ae8dc87b
Vukasin Dimitrijevic <vule.dimitrijevic20@gmail.com>
2025-11-07 21:47:57 +0100

    Create CardComponent to display individual product details

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,35 @@
-import { useEffect } from "react"
-import ProductService from "../services/ProductServices"
+import { useEffect } from "react";
+import ProductService from "../services/ProductServices";
 // Redux
-import { useDispatch, useSelector } from "react-redux"
-import { saveAllProductsAction } from "../store/productSlice"
+import { useDispatch, useSelector } from "react-redux";
+import { saveAllProductsAction } from "../store/productSlice";
+import CardComponent from "../components/CardComponent";
+
 function HomePage() {
   const { allProducts, isLoading } = useSelector((state) => state.productStore);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     ProductService.getAllProductsService()
-    .then((res) => {
-      dispatch(saveAllProductsAction(res.data.products))
-    })
-    .catch((err) => console.log(err))
-  }, [])
-
+      .then((res) => {
+        dispatch(saveAllProductsAction(res.data.products));
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <div>HomePage</div>
-  )
+    <div>
+      {isLoading ? (
+        <div>
+          {allProducts.map((product) => {
+            return <CardComponent key={product.id} product={product} />;
+          })}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
