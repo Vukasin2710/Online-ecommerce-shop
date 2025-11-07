46352c3788cde213014c60c03389d130ae940c07
Vukasin Dimitrijevic <vule.dimitrijevic20@gmail.com>
2025-11-07 21:38:39 +0100

    reate productSlice with saveAllProductsAction to store products in Redux

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,21 @@
-
+import { useEffect } from "react"
+import ProductService from "../services/ProductServices"
+// Redux
+import { useDispatch, useSelector } from "react-redux"
+import { saveAllProductsAction } from "../store/productSlice"
 function HomePage() {
+  const { allProducts, isLoading } = useSelector((state) => state.productStore);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    ProductService.getAllProductsService()
+    .then((res) => {
+      dispatch(saveAllProductsAction(res.data.products))
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
+
   return (
     <div>HomePage</div>
   )
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 // slices
 import categorySlice from "./categorySlice";
+import productSlice from "./productSlice";
 
 const store = configureStore({
   reducer: {
     categoryStore: categorySlice,
+    productStore: productSlice
   },
 });
 
