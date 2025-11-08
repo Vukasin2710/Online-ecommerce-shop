514a965de78e3e0d5aabbbc98c0ebb123f5d8a88
Vukasin Dimitrijevic <vule.dimitrijevic20@gmail.com>
2025-11-09 00:15:11 +0100

    Complete SingleProductPage and add CartPage.

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 class ProductService {
   static getAllProductsService = () => axios.get('/products?limit=30&skip=80');
+  static getSingleProduct = (id) => axios.get(`/products/${id}`);
 }
 
 export default ProductService;
+
+
 
